37febc606d01abaf9b08c5aacc5817d239c8346f
pedrox10 <barcocarrasco.pedro@gmail.com>
2025-11-27 18:00:35 -0400

    organigram segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatNavList} from "@angular/material/list";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {FuncionariosComponent} from "./funcionarios/funcionarios.component";
+import {AppComponent} from "./app.component";
+import {InicioComponent} from "./inicio/inicio.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent, // <--- AQUÍ EL LAYOUT
+    children: [
+      {
+        path: 'funcionarios',
+        component: FuncionariosComponent
+      },
+      {
+        path: '',
+        redirectTo: 'funcionarios',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
